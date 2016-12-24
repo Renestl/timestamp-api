bfd15dac1070c8bdceee700cf9037bfd69228ae5
Jennifer Currie <renestl@gmail.com>
2016-12-23 21:51:40 -0600

    timestamp complete

--- README.md
+++ README.md
@@ -2,9 +2,21 @@
 
 Project created for FreeCodeCamp Backend challenges.
 
+** by Jennifer Currie **
+
+[GitHub](https://github.com/Renestl) | [FreeCodeCamp](https://www.freecodecamp.com/renestl) | [Codepen](http://codepen.io/renestl/pen/ORdNKZ) | [LinkedIn]() | [Twitter](@robinskrizan)
+
 ### Technologies Used
 
 NodeJS
 
+###### Objectives
+Build a Heroku app that is functionally similar to this: https://timestamp-ms.herokuapp.com/.
+
+###### User Stories
+* I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016)
+* If it does, it returns both the Unix timestamp and the natural language form of that date.
+* If it does not contain a date or Unix timestamp, it returns null for those properties.
+
 ### Project links:
-* [Github Repository]()
+* [Github Repository](https://github.com/Renestl/timestamp-api)
--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 var express = require('express');
 app = express();
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	res.render('home');
 });
 
+app.get('/:time', function(req, res) {
+
+	// human readable time
+	function unixToNatural(unix) {
+	var date = new Date(unix * 1000);
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    var month = months[date.getMonth()];
+    var day = date.getDate();
+    var year = date.getFullYear();
+
+    var result = month + ' ' + day + ', ' + year;
+    return result;
+  }
+
+ if(!isNaN(req.params.time)) {
+    var result = unixToNatural(req.params.time);
+    var data = {unix: req.params.time, natural: result};
+    res.json(data);
+  } 
+  	else  {
+		var natural = new Date(req.params.time);
+		
+		if(!isNaN(natural)) {
+			var unix = natural / 1000;
+			var data = {unix: unix, natural: req.params.time};
+			res.json(data);
+      } 
+	  	else {
+        res.json({unix: null, natural: null});
+    	}
+  }
+
+});
 
 app.listen(3000, '127.0.0.1', function() {
 	console.log('Server has started!');
