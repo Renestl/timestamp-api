5ddb609a00578ddca5d84b9ccf5b7473eaba8622
Jennifer Currie <renestl@gmail.com>
2016-12-24 01:42:10 -0600

    changed listening port

--- app.js
+++ app.js
@@ -43,6 +43,13 @@ app.get('/:time', function(req, res) {
 
 });
 
-app.listen(3000, '127.0.0.1', function() {
-	console.log('Server has started!');
-});
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
+
+// app.listen(3000, '127.0.0.1', function() {
+// 	console.log('Server has started!');
+// });
