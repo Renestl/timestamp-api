ced45d0981fa05091a62f70e6d87ea3be8c67e04
Jennifer Currie <renestl@gmail.com>
2017-01-06 04:25:47 -0600

    uncomment server start

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ app.get('/:time', function(req, res) {
 
 });
 
-  // var port = process.env.PORT || 3000;
-  // app.listen(port);
+  var port = process.env.PORT || 3000;
+  app.listen(port);
 
 // app.listen(3000, '127.0.0.1', function() {
 // 	console.log('Server has started!');
